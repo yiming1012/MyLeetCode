f7c4a04d00b1ec21ca638230eaac87f20de9cf27
yiming <1129079384@qq.com>
2020-05-14 20:01:59 +0800

    二叉树

---
+++ LeetCode/树(Binary Tree)/111. Minimum Depth of Binary Tree.py
@@ -0,0 +1,59 @@
+"""
+Given a binary tree, find its minimum depth.
+
+The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Given binary tree [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its minimum depth = 2.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/minimum-depth-of-binary-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+# Definition for a binary tree node.
+import collections
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def minDepth(self, root: TreeNode) -> int:
+        """
+        执行用时 :52 ms, 在所有 Python3 提交中击败了80.43%的用户
+        内存消耗 :14.9 MB, 在所有 Python3 提交中击败了12.50%的用户
+        思路：
+        1、树的最小深度指的是，从根节点到叶子节点的高度
+        2、叶子节点即左右子节点都为空
+        3、层次遍历，遇到叶子节点则返回高度
+        """
+        if not root:
+            return 0
+        queue = collections.deque()
+        queue.append(root)
+        count = 1
+        while queue:
+            for _ in range(len(queue)):
+                root = queue.popleft()
+                if not root.left and not root.right:
+                    return count
+                if root.left:
+                    queue.append(root.left)
+                if root.right:
+                    queue.append(root.right)
+            else:
+                count += 1
---
+++ LeetCode/树(Binary Tree)/160. Intersection of Two Linked Lists.py
@@ -0,0 +1,90 @@
+"""
+Write a program to find the node at which the intersection of two singly linked lists begins.
+
+For example, the following two linked lists:
+
+
+begin to intersect at node c1.
+
+ 
+
+Example 1:
+
+
+Input: intersectVal = 8, listA = [4,1,8,4,5], listB = [5,0,1,8,4,5], skipA = 2, skipB = 3
+Output: Reference of the node with value = 8
+Input Explanation: The intersected node's value is 8 (note that this must not be 0 if the two lists intersect). From the head of A, it reads as [4,1,8,4,5]. From the head of B, it reads as [5,0,1,8,4,5]. There are 2 nodes before the intersected node in A; There are 3 nodes before the intersected node in B.
+ 
+
+Example 2:
+
+
+Input: intersectVal = 2, listA = [0,9,1,2,4], listB = [3,2,4], skipA = 3, skipB = 1
+Output: Reference of the node with value = 2
+Input Explanation: The intersected node's value is 2 (note that this must not be 0 if the two lists intersect). From the head of A, it reads as [0,9,1,2,4]. From the head of B, it reads as [3,2,4]. There are 3 nodes before the intersected node in A; There are 1 node before the intersected node in B.
+ 
+
+Example 3:
+
+
+Input: intersectVal = 0, listA = [2,6,4], listB = [1,5], skipA = 3, skipB = 2
+Output: null
+Input Explanation: From the head of A, it reads as [2,6,4]. From the head of B, it reads as [1,5]. Since the two lists do not intersect, intersectVal must be 0, while skipA and skipB can be arbitrary values.
+Explanation: The two lists do not intersect, so return null.
+ 
+
+Notes:
+
+If the two linked lists have no intersection at all, return null.
+The linked lists must retain their original structure after the function returns.
+You may assume there are no cycles anywhere in the entire linked structure.
+Your code should preferably run in O(n) time and use only O(1) memory.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/intersection-of-two-linked-lists
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        a, b = [], []
+        while headA:
+            a.append(headA)
+            headA = headA.next
+        while headB:
+            b.append(headB)
+            headB = headB.next
+
+        res = None
+        while a and b and a[-1] == b[-1]:
+            res = a.pop()
+            b.pop()
+        return res
+
+    def getIntersectionNode2(self, headA: ListNode, headB: ListNode) -> ListNode:
+        """
+        思路：
+        一种比较巧妙的方式是，分别为链表A和链表B设置指针A和指针B，然后开始遍历链表，如果遍历完当前链表，则将指针指向另外一个链表的头部继续遍历，直至两个指针相遇。
+        最终两个指针分别走过的路径为：
+        指针A :a+c+b
+        指针B :b+c+a
+        明显 a+c+b = b+c+a,因而如果两个链表相交，则指针A和指针B必定在相交结点相遇。
+        如果两次走完中间都没有遇到交点，则两个点都会指向
+        """
+        if not headA or not headB:
+            return None
+        p1 = headA
+        p2 = headB
+        while p1 != p2:
+            p1 = p1.next if p1 else headB
+            p2 = p2.next if p2 else headA
+        return p1
+
