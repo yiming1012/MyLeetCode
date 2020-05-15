a1cecbd5f06f114796730607e4ae41754f6011d2
yiming <1129079384@qq.com>
2020-05-15 10:45:36 +0800

    哈希表

--- LeetCode/哈希表(hash table)/560. Subarray Sum Equals K.py
+++ LeetCode/哈希表(hash table)/560. Subarray Sum Equals K.py
@@ -18,21 +18,21 @@ class Solution:
         '''
         执行用时 :140 ms, 在所有 Python3 提交中击败了43.94%的用户
         内存消耗 :16.1 MB, 在所有 Python3 提交中击败了5.45%的用户
-        思路：
+        思路：前缀和+哈希表
             1、累加每一个数，添加到dict中
-            2、巧妙之处在于，dict[0]=1.因为count+=dict[cumsum-k]，当前n个和为k时，cumsum-k=0
+            2、巧妙之处在于，dict[0]=1.因为count+=dict[presum-k]，当前n个和为k时，presum-k=0，避免了单独对presum==k进行判断
         :param nums:
         :param k:
         :return:
         '''
         dic = collections.defaultdict(lambda: 0)
         dic[0] = 1
-        cumsum, count = 0, 0
+        presum, count = 0, 0
         for i in range(len(nums)):
-            cumsum += nums[i]
-            if cumsum - k in dic:
-                count += dic[cumsum - k]
-            dic[cumsum] += 1
+            presum += nums[i]
+            if presum - k in dic:
+                count += dic[presum - k]
+            dic[presum] += 1
         return count
 
 
