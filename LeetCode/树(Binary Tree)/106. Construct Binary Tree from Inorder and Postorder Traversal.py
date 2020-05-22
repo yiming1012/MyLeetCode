a354cbe270491fdf60ce56a988af4a7fc63ad7a4
yiming <1129079384@qq.com>
2020-05-22 10:31:46 +0800

    树

---
+++ LeetCode/树(Binary Tree)/106. Construct Binary Tree from Inorder and Postorder Traversal.py
@@ -0,0 +1,46 @@
+"""
+Given inorder and postorder traversal of a tree, construct the binary tree.
+
+Note:
+You may assume that duplicates do not exist in the tree.
+
+For example, given
+
+inorder = [9,3,15,20,7]
+postorder = [9,15,7,20,3]
+Return the following binary tree:
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+# Definition for a binary tree node.
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
+        if not inorder:
+            return None
+        root = postorder[-1]
+        i = inorder.index(root)
+        root = TreeNode(root)
+        root.left = self.buildTree(inorder[:i], postorder[:i])
+        root.right = self.buildTree(inorder[i + 1:], postorder[i:len(postorder) - 1])
+        return root
+
+
