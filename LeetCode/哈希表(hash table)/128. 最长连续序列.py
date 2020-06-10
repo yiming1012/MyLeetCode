85cabfffb3174b57f87042bba191f28ddabe02e1
yiming1012 <1129079384@qq.com>
2020-06-10 23:05:05 +0800

    哈希表

---
+++ LeetCode/哈希表(hash table)/128. 最长连续序列.py
@@ -0,0 +1,38 @@
+"""
+给定一个未排序的整数数组，找出最长连续序列的长度。
+
+要求算法的时间复杂度为 O(n)。
+
+示例:
+
+输入: [100, 4, 200, 1, 3, 2]
+输出: 4
+解释: 最长连续序列是 [1, 2, 3, 4]。它的长度为 4。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/longest-consecutive-sequence
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+import collections
+from typing import List
+
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        hash_dict = dict()
+
+        max_length = 0
+        for num in nums:
+            if num not in hash_dict:
+                left = hash_dict.get(num - 1, 0)
+                right = hash_dict.get(num + 1, 0)
+
+                cur_length = 1 + left + right
+                if cur_length > max_length:
+                    max_length = cur_length
+
+                hash_dict[num] = cur_length
+                hash_dict[num - left] = cur_length
+                hash_dict[num + right] = cur_length
+
+        return max_length
