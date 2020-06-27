bddaa9ea4ba3b9b457bd60d99b99a37fe9135d87
yiming <1129079384@qq.com>
2020-06-27 17:29:16 +0800

    数组

--- LeetCode/数组/448. 找到所有数组中消失的数字.py
+++ LeetCode/数组/448. 找到所有数组中消失的数字.py
@@ -43,8 +43,8 @@ class Solution:
         """
         res = []
         for i in range(len(nums)):
-            if nums[abs(nums[i])-1] > 0:
-                nums[abs(nums[i])-1] = -nums[abs(nums[i])-1]
+            if nums[abs(nums[i]) - 1] > 0:
+                nums[abs(nums[i]) - 1] *= -1
 
         for i in range(len(nums)):
             if nums[i] > 0:
