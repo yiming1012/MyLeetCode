a977670e4ee32095ee4f48d37e8de5c1f6ff03a5
yiming <1129079384@qq.com>
2020-07-16 20:33:45 +0800

    -

--- LeetCode/动态规划法(dp)/96. 不同的二叉搜索树.py
+++ LeetCode/动态规划法(dp)/96. 不同的二叉搜索树.py
@@ -19,6 +19,7 @@
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 """
 import math
+from functools import lru_cache
 
 
 class Solution:
@@ -29,16 +30,35 @@ class Solution:
         return int(math.factorial(n * 2) / math.factorial(n) ** 2 / (n + 1))
 
     def numTrees2(self, n: int) -> int:
+        """
+        动态规划
+        假设n个节点存在二叉排序树的个数是G(n)，令f(i)为以i为根的二叉搜索树的个数
+        即有:G(n) = f(1) + f(2) + f(3) + f(4) + ... + f(n)
+        n为根节点，当i为根节点时，其左子树节点个数为[1,2,3,...,i-1]，右子树节点个数为[i+1,i+2,...n]，
+        所以当i为根节点时，其左子树节点个数为i-1个，右子树节点为n-i，即f(i) = G(i-1)*G(n-i),
+        上面两式可得:G(n) = G(0)*G(n-1)+G(1)*(n-2)+...+G(n-1)*G(0)
+        """
         dp = [0] * (n + 1)
         dp[0] = 1
         dp[1] = 1
         for i in range(2, n + 1):
-            for j in range(1, i + 1):
-                dp[i] += dp[j - 1] * dp[i - j]
+            for j in range(i):
+                dp[i] += dp[j] * dp[i - j - 1]
         return dp[n]
+
+    @lru_cache()
+    def numTrees3(self, n: int) -> int:
+        """
+        思路：递归+备忘录
+        """
+        if n <= 0: return 1
+        if n <= 2: return n
+
+        return sum([self.numTrees(i - 1) * self.numTrees(n - i) for i in range(1, n + 1)])
 
 
 if __name__ == '__main__':
-    n = 5
+    n = 8
     print(Solution().numTrees1(n))
     print(Solution().numTrees2(n))
+    print(Solution().numTrees3(n))
