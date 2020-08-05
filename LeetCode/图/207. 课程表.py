fa07c4944027bd083fbfcf3c997e45e01d83730f
yiming <1129079384@qq.com>
2020-08-05 09:46:00 +0800

    -

---
+++ LeetCode/图/207. 课程表.py
@@ -0,0 +1,64 @@
+"""
+你这个学期必须选修 numCourse 门课程，记为 0 到 numCourse-1 。
+
+在选修某些课程之前需要一些先修课程。 例如，想要学习课程 0 ，你需要先完成课程 1 ，我们用一个匹配来表示他们：[0,1]
+
+给定课程总量以及它们的先决条件，请你判断是否可能完成所有课程的学习？
+
+ 
+
+示例 1:
+
+输入: 2, [[1,0]]
+输出: true
+解释: 总共有 2 门课程。学习课程 1 之前，你需要完成课程 0。所以这是可能的。
+示例 2:
+
+输入: 2, [[1,0],[0,1]]
+输出: false
+解释: 总共有 2 门课程。学习课程 1 之前，你需要先完成​课程 0；并且学习课程 0 之前，你还应先完成课程 1。这是不可能的。
+ 
+
+提示：
+
+输入的先决条件是由 边缘列表 表示的图形，而不是 邻接矩阵 。详情请参见图的表示法。
+你可以假定输入的先决条件中没有重复的边。
+1 <= numCourses <= 10^5
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/course-schedule
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+import collections
+from typing import List
+
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        """
+        拓扑排序模板
+        """
+        edges = collections.defaultdict(list)
+        indeg = [0] * numCourses
+
+        for info in prerequisites:
+            edges[info[1]].append(info[0])
+            indeg[info[0]] += 1
+
+        queue = collections.deque([u for u in range(numCourses) if indeg[u] == 0])
+        visited = 0
+
+        while queue:
+            visited += 1
+            u = queue.popleft()
+            for v in edges[u]:
+                indeg[v] -= 1
+                if indeg[v] == 0:
+                    queue.append(v)
+
+        return visited == numCourses
+
+
+if __name__ == '__main__':
+    numCourses, prerequisites = 2, [[1, 0]]
+    print(Solution().canFinish(numCourses, prerequisites))
