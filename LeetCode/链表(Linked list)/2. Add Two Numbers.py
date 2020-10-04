1ad4adc605b2c3f1c8cb4212419de70502aefdf5
yiming1012 <1129079384@qq.com>
2020-10-04 22:32:20 +0800

    -

--- LeetCode/链表(Linked list)/2. Add Two Numbers.py
+++ LeetCode/链表(Linked list)/2. Add Two Numbers.py
@@ -10,18 +10,16 @@ class ListNode:
 
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        dummy = result = ListNode(0)
-        flag = 0
-        while l1 or l2 or flag:
-            two_sum = (l1.val if l1 else 0) + (l2.val if l2 else 0) + flag
-            flag, mod = divmod(two_sum, 10)
-            result.next = ListNode(mod)
-
-            if l1:
-                l1 = l1.next
-            if l2:
-                l2 = l2.next
-            result = result.next
+        carry = 0
+        node = ListNode(0)
+        dummy = node
+        while l1 or l2 or carry:
+            num = carry + (l1.val if l1 else 0) + (l2.val if l2 else 0)
+            carry, num = divmod(num, 10)
+            node.next = ListNode(num)
+            node = node.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
 
         return dummy.next
 
