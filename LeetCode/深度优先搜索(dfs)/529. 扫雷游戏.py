ea665fa8f93db359dfb483e6f700d438be8d5735
yiming <1129079384@qq.com>
2020-10-30 10:50:03 +0800

    -

--- LeetCode/深度优先搜索(dfs)/529. 扫雷游戏.py
+++ LeetCode/深度优先搜索(dfs)/529. 扫雷游戏.py
@@ -64,6 +64,9 @@ Click : [1,2]
 链接：https://leetcode-cn.com/problems/minesweeper
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 """
+from typing import List
+
+
 class Solution:
     def updateBoard(self, board: List[List[str]], click: List[int]) -> List[List[str]]:
          # 定义 8 个方位
