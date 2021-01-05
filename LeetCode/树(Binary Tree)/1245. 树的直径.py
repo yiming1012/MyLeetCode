7c719875d5510036f580216936f605427ed73890
yiming1012 <1129079384@qq.com>
2021-01-05 13:04:45 +0800

    -

---
+++ LeetCode/树(Binary Tree)/1245. 树的直径.py
@@ -0,0 +1,61 @@
+"""
+1245. 树的直径
+给你这棵「无向树」，请你测算并返回它的「直径」：这棵树上最长简单路径的 边数。
+
+我们用一个由所有「边」组成的数组 edges 来表示一棵无向树，其中 edges[i] = [u, v] 表示节点 u 和 v 之间的双向边。
+
+树上的节点都已经用 {0, 1, ..., edges.length} 中的数做了标记，每个节点上的标记都是独一无二的。
+
+
+
+示例 1：
+
+
+
+输入：edges = [[0,1],[0,2]]
+输出：2
+解释：
+这棵树上最长的路径是 1 - 0 - 2，边数为 2。
+示例 2：
+
+
+
+输入：edges = [[0,1],[1,2],[2,3],[1,4],[4,5]]
+输出：4
+解释：
+这棵树上最长的路径是 3 - 2 - 1 - 4 - 5，边数为 4。
+
+
+提示：
+
+0 <= edges.length < 10^4
+edges[i][0] != edges[i][1]
+0 <= edges[i][j] <= edges.length
+edges 会形成一棵无向树
+"""
+
+
+class Solution:
+    def treeDiameter(self, edges: List[List[int]]) -> int:
+        graph = collections.defaultdict(list)
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+        visited = set()
+        res = 0
+
+        def dfs(root):
+            nonlocal res
+            pre = cur = 0
+            visited.add(root)
+            for nex in graph[root]:
+                if nex not in visited:
+                    cur = 1 + dfs(nex)
+
+                    res = max(res, cur + pre)
+                    if cur > pre:
+                        pre = cur
+            return pre
+
+        dfs(0)
+        return res
